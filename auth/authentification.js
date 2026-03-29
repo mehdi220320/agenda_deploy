90c394e72aa1c23809838e3a1a37264b35347aa4
Mohamed Mehdi Ben Gamra <111698017+mehdi220320@users.noreply.github.com>
2026-03-29 15:59:39 +0100

    Add family option to nodemailer transport config

--- auth/authentification.js
+++ auth/authentification.js
@@ -153,6 +153,7 @@ router.post('/forgot-password', async (req, res) => {
             host: 'smtp.gmail.com',
             port: 587,
             secure: false,
+            family: 4,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASSWORD
