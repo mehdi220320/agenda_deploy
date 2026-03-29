6c44a1ca07935ccd5bf18e5936ee7a5c40d12dbc
Mohamed Mehdi Ben Gamra <111698017+mehdi220320@users.noreply.github.com>
2026-03-29 14:25:04 +0100

    Update Sequelize configuration for database connection
    Refactor database connection to use DATABASE_URL with SSL options.

--- config/db.js
+++ config/db.js
@@ -1,16 +1,15 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-    process.env.DB_DATABASE,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT ,
-        dialect: "postgres",
-        logging: false,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
     }
-);
+});
 
 module.exports = sequelize;
