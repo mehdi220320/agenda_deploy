f2b1cf6ed13006b70b54ecdf7de148141411b394
Mohamed Mehdi Ben Gamra <111698017+mehdi220320@users.noreply.github.com>
2026-04-23 18:02:51 +0100

    Fix connection initialization for IORedis

--- config/queue.js
+++ config/queue.js
@@ -1,8 +1,9 @@
 const { Queue } = require("bullmq");
 const IORedis = require("ioredis");
 
-const connection = new IORedis(process.env.UPSTASH_REDIS_URL,maxRetriesPerRequest: null);
-
+const connection = new IORedis(process.env.UPSTASH_REDIS_URL, {
+    maxRetriesPerRequest: null
+});
 const alarmQueue = new Queue("alarm-queue", { connection });
 
 module.exports = { alarmQueue, connection };
